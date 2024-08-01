cf263d2795a2522f85ba11fa91c6592c906f1a9f
Armando Mejia <armandor.mejia4@yahoo.com>
2024-08-01 15:33:05 -0700

    secret code

---
+++ script .js
@@ -0,0 +1,20 @@
+/*
+create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+add variable typeof 10
+add variable tyeoof 40
+add variable typeof 39
+create a dialog popup in browser with lock combination
+*/
+
+//created a string for the message
+let str = "You have received this message because you have been chosen to open an important vault.Here is the secret combination:";
+
+// dividing to get 10
+let num1 = 20 / 2;
+//multiplying to get 40
+let num2 = 8 * 5;
+//adding to get 39
+let num3 = 23 + 16;
+
+// creating a poppup dialog in browser for complete message
+alert(`${str} ${num1}-${num2}-${num3} `)
